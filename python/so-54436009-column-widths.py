a0af296b63401e2edd0c066bbdbe35a087b099c8
Steven Hunt <sph@commandersoft.co.uk>
2019-08-02 15:13:31 +0100

    Update Python samples

--- python/so-54436009-column-widths.py
+++ python/so-54436009-column-widths.py
@@ -1,11 +1,22 @@
 from openpyxl import Workbook, load_workbook
 #from openpyxl.utils import get_column_letter
+#from openpyxl.worksheet.dimensions import SheetFormatProperties
 
 ### Simple script scribblings to help with the answer to SO post
 ### https://stackoverflow.com/questions/54436009
 
-wbP = load_workbook("c:\\temp\\test.xlsx")
-wsP = wbP.active
+#wbP = load_workbook("c:\\temp\\payments-upload.xlsx")
+#wsP = wbP.active
+#wsP = wbP["Employees"]
+
+wb1 = load_workbook("c:\\temp\\col-width-1.xlsx")
+wb2 = load_workbook("c:\\temp\\col-width-2.xlsx")
+
+ws1 = wb1.active
+ws2 = wb2.active
+
+print("WS1 sheet props: ", ws1.sheet_format.defaultColWidth)
+print("WS2 sheet props: ", ws2.sheet_format.defaultColWidth)
 
 #print(wb.sheetnames)
 #print(ws.title)
@@ -17,20 +28,30 @@ wsP = wbP.active
 #     ws = wb[title]
 #     print(coord)
 
-col_b_w = wsP.column_dimensions['B'].width
-col_e_w = wsP.column_dimensions['E'].width
-print("Col B width: ", col_b_w)
-print("Col E width: ", col_e_w)
+#col_b_w = wsP.column_dimensions['B'].width
+#col_e_w = wsP.column_dimensions['E'].width
+#print("Col B width: ", col_b_w)
+#print("Col E width: ", col_e_w)
 
-wb = Workbook()
-ws1 = wb.active
-ws1.title = "SO_Test1"
+#wb = Workbook()
+#ws1 = wb.active
+#ws1.title = "SO_Test1"
 
 #ws2 = wb.create_sheet("P4")
 
-ws1.column_dimensions['B'].width = 30
-if (col_e_w != None):
-    ws1.column_dimensions['E'].width = col_e_w
+#ws1.column_dimensions['B'].width = 30
+## My SO answer
+#if (col_e_w is not None):
+#    ws1.column_dimensions['E'].width = col_e_w
+
+## A better answer, perhaps
+#for k, cd in wsP.column_dimensions.items():
+#    if cd is not None and cd.width != 0:
+#        ws1.column_dimensions[k].width = cd.width
+
+#col_e_w = wsP.column_dimensions['E'].width
+
+#ws1.column_dimensions['E'].width = None
 
 #ws1['A1'] = "Hello"
 #ws1['B2'] = "World"
@@ -44,4 +65,4 @@ if (col_e_w != None):
 #i = get_column_letter(2)
 #ws1.column_dimensions[i].width = 20
 
-wb.save(filename = "c:\\temp\\so-test4.xlsx")
+#wb.save(filename = "c:\\temp\\so-test4.xlsx")
